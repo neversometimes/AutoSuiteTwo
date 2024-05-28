a81325e2fbf079e3e93fde208f86a11179c6da7f
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-05-28 14:57:24 -0700

    Cleanup InfiniteScrollPage.

--- src/test/java/pages/InfiniteScrollPage.java
+++ src/test/java/pages/InfiniteScrollPage.java
@@ -2,18 +2,15 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class InfiniteScrollPage {
 
     private WebDriver driver;
     private By lastInitPara = By.xpath("//div[@class='jscroll-added'][2]");
-    private By genericPara = By.xpath("//div[@class='jscroll-inner']//child::div");
     private By thePara = By.className("jscroll-added");
 
     public InfiniteScrollPage (WebDriver driver) {
@@ -24,8 +21,6 @@ public class InfiniteScrollPage {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(lastInitPara));
-
-        WebElement foo = driver.findElement(lastInitPara);
 
         Actions actions = new Actions(driver);
 
