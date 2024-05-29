b24677f8ef58732de8ce738bae697b8798611004
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-05-29 16:57:09 -0700

    Re-wrote test to be more stable. Will not work on Safari.

--- src/test/java/pages/ContextMenuPage.java
+++ src/test/java/pages/ContextMenuPage.java
@@ -1,12 +1,7 @@
 package pages;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -20,25 +15,21 @@ public class ContextMenuPage {
         this.driver = driver;
     }
 
-    public void getContextMenu() {
+    public String getContextMenu() {
         Actions actions = new Actions(driver);
 
         WebElement contextMenu = driver.findElement(rightClickTarget);
-        actions.contextClick(contextMenu).build().perform();
 
-    }
-
-    public String isContextMenuDisplayed () {
-        // alert shows if right-click happens within page target region
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.alertIsPresent());
+        actions.moveToElement(contextMenu)
+                .contextClick()
+                .pause(Duration.ofSeconds(3))
+                .perform();
 
         Alert alert = driver.switchTo().alert();
 
-        return alert.getText();  // return alert text to verify it is visible
+        String alertTxt = alert.getText();
+        alert.accept();
+        return alertTxt;
+
     }
-
-
-
 }
--- src/test/java/tests/ContextMenuTest.java
+++ src/test/java/tests/ContextMenuTest.java
@@ -1,26 +1,26 @@
 package tests;
 
 import base.BaseTests;
-
 import org.testng.annotations.Test;
 import pages.ContextMenuPage;
 import static org.testng.Assert.*;
 
 public class ContextMenuTest extends BaseTests{
 
+    // NOTE:  This test will not pass in Safari.  The JS alert handling doesn't work.
+
     @Test
     public void verifyContextMenus() {
-
-        driver.get("https://the-internet.herokuapp.com/context_menu");
+        String homePage = "https://the-internet.herokuapp.com/context_menu";
+        driver.get(homePage);
 
         ContextMenuPage contextMenuPage = new ContextMenuPage(driver);
 
         // right-click w/in specific region of page to display context menu verification alert
-        contextMenuPage.getContextMenu();
+        assertEquals(contextMenuPage.getContextMenu(), "You selected a context menu");
 
-        // verify context menu alert text is displayed
-        assertEquals(contextMenuPage.isContextMenuDisplayed(), "You selected a context menu");
-
+        // verify dismiss alert back to start page
+        assertEquals(driver.getCurrentUrl(), homePage);
     }
 
 }
