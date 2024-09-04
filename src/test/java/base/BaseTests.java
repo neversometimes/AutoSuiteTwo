e8cef3e7ac9ebe7c7b2beab7738d213e72821b06
Brian <neversometimes@icloud.com>
2024-09-04 14:34:27 -0700

    removed safari references and other unused references.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,8 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class BaseTests {
 
@@ -17,19 +18,16 @@ public class BaseTests {
     void setupClass() {
         WebDriverManager.edgedriver().setup();
         WebDriverManager.firefoxdriver().setup();
-        //WebDriverManager.safaridriver().setup();
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
     @Parameters("browser")
-    public void setup(String browser) throws Exception {  // "String browser" as setup parameters
+    public void setup(String browser) throws Exception {  // "String browser" as setup parameter
 
         if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
-        } /*else if (browser.equalsIgnoreCase("safari")) {
-            driver = new SafariDriver();
-        }*/ else if (browser.equalsIgnoreCase("firefox")) {
+        } else if (browser.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
@@ -37,8 +35,8 @@ public class BaseTests {
             throw new Exception("Incorrect Browser");
         }
 
-         // driver = new FirefoxDriver();  // used while developing tests
-
+          // driver = new FirefoxDriver();  // used while developing tests
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterMethod
